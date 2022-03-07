78e57a6482ba6303a17dc83c012f8b30be4ce935
atzisb-kozak <alkergoat@gmail.com>
2022-03-07 13:01:19 +0100

    [AKT] feat(exo4) resolve first async io

---
+++ Part 1/LearnYouNode/my-first-async-io.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+function ReadFile(file) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, {}, (err, data) => {
+			if (err) return reject(new Error(`${file} not found`));
+			return resolve(data.toString());
+		})
+	}) 
+}
+
+ReadFile(process.argv[2]).then((result) => {
+	console.log(result.split('\n').length);
+}, (err) => {
+	console.log(err.message);
+});
